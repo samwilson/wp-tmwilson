cf9749ad91854e3e122705e77ef5b6e950d2736d
Sam Wilson <sam@samwilson.id.au>
2017-10-06 13:52:51 +0800

    Front page

--- background-image.php
+++ background-image.php
@@ -3,7 +3,6 @@
 Template Name: background-image
 */
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,40 +16,39 @@ Template Name: background-image
 	<title>
 	<?php bloginfo('name'); ?> <?php wp_title(); ?>
 	</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css"
+		  integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous" />
 	<style type="text/css">
-		* { padding: 0; margin: 0; font-size:110%;
-			font-family: Corbel, "Lucida Grande", "Lucida Sans Unicode", "Lucida Sans", "DejaVu Sans", "Bitstream Vera Sans", "Liberation Sans", Verdana, "Verdana Ref", sans-serif;
-		}
-		p { color: #fff; font-size: 16px; line-height: 24px; margin-bottom: 22px; -webkit-font-smoothing: antialiased; }
-		a { color: #fff; text-decoration: none }
+		a, h1, div, p { color:white }
 		a:hover { color: rgb(84,66,47); background-color:rgba(255,255,255,0.3); }
-		h1 { padding: 50px 0 0 0; margin: 0 auto 0 50px; color:white; letter-spacing:0.12em; font-size:2.7em;
-			font-family: Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans-serif;
-		}
-		#content { width: 360px; padding: 0 0 0 150px; margin: -35px 50px 0 auto !important; text-align:justify }
-		#read-on { position:absolute; bottom:50px; right:50px; width:360px; text-align:center; font-size:120% }
-		#read-on a { padding:0.1em; border:1px solid rgba(255,255,255,0.3); }
+		#read-on { margin-top:4em; text-align:right }
+		#read-on a { border:1px solid rgba(255,255,255,0.3); padding:0.2em; font-size:larger }
 	</style>
 	<?php wp_head(); ?>
 </head>
 
 <body>
 
-	<h1><?php bloginfo('name'); ?></h1>
+	<main class="container-fluid">
+		<div class="row">
+			<header class="col-md-8 col-12">
+				<h1 class=""><?php bloginfo('name') ?></h1>
+				<p class="mt-4"><?php bloginfo('description') ?></p>
+			</header>
+			<article class="col-md-4 col-12">
+				<?php if (have_posts()) : ?>
+				<?php while (have_posts()) : the_post(); ?>
+				<div class="text-justify"><?php the_content() ?></div>
+				<p class="" id="read-on"><a href="/about">Read on…</a></p>
+				<?php endwhile; endif ?>
+			</article>
+		</div>
+	</main>
 
-	<?php if (have_posts()) : ?>
-		<?php while (have_posts()) : the_post(); ?>
-
-		<div id="content"><?php the_content() ?></div>
-		<p id="read-on"><a href="/about">Read on…</a></p>
-
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js" integrity="sha256-V52dl3OFjoY+fYAkifhLJ7f1V7mZAKPGCQoWzoQxrEU=" crossorigin="anonymous"></script>
-		<script type="text/javascript">
-			$.backstretch("<?php bloginfo('template_directory'); ?>/images/front-page.jpg");
-		</script>
-
-	<?php endwhile; endif ?>
-
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js" integrity="sha256-DZAnKJ/6XZ9si04Hgrsxu/8s717jcIzLy3oi35EouyE=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-backstretch/2.0.4/jquery.backstretch.min.js" integrity="sha256-V52dl3OFjoY+fYAkifhLJ7f1V7mZAKPGCQoWzoQxrEU=" crossorigin="anonymous"></script>
+	<script type="text/javascript">
+		$.backstretch("<?php bloginfo('template_directory'); ?>/images/front-page.jpg");
+	</script>
 </body>
 </html>
